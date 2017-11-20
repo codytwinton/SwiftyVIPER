bfdcfebd8bd6eb3ce808d1c206b89381b20012fb
Cody Winton <cody.t.winton@gmail.com>
2017-11-19 20:37:53 -0600

    Updated Templates based on best practices

--- Templates/VIPER/Module.xctemplate/___FILEBASENAME___Module.swift
+++ Templates/VIPER/Module.xctemplate/___FILEBASENAME___Module.swift
@@ -10,42 +10,43 @@
 
 // MARK: Imports
 
-import UIKit
-
 import SwiftyVIPER
+import UIKit
 
 // MARK: -
 
 /// Used to initialize the ___FILEBASENAMEASIDENTIFIER___ VIPER module
-final class ___FILEBASENAMEASIDENTIFIER___Module {
+final class ___FILEBASENAMEASIDENTIFIER___Module: ModuleProtocol {
 
 	// MARK: - Variables
 
 	private(set) lazy var interactor: ___FILEBASENAMEASIDENTIFIER___Interactor = {
-		return ___FILEBASENAMEASIDENTIFIER___Interactor()
+		___FILEBASENAMEASIDENTIFIER___Interactor()
 	}()
 
 	private(set) lazy var router: ___FILEBASENAMEASIDENTIFIER___Router = {
-		return ___FILEBASENAMEASIDENTIFIER___Router()
+		___FILEBASENAMEASIDENTIFIER___Router()
 	}()
 
 	private(set) lazy var presenter: ___FILEBASENAMEASIDENTIFIER___Presenter = {
-		return ___FILEBASENAMEASIDENTIFIER___Presenter(router: self.router, interactor: self.interactor)
+		___FILEBASENAMEASIDENTIFIER___Presenter(router: self.router, interactor: self.interactor)
 	}()
 
 	private(set) lazy var view: ___FILEBASENAMEASIDENTIFIER___ViewController = {
-		return ___FILEBASENAMEASIDENTIFIER___ViewController(presenter: self.presenter)
+		___FILEBASENAMEASIDENTIFIER___ViewController(presenter: self.presenter)
 	}()
+
+	// MARK: - Module Protocol Variables
+
+	var viewController: UIViewController {
+		return view
+	}
+
+	// MARK: Inits
 
 	init() {
 		presenter.view = view
 		router.viewController = view
 		interactor.presenter = presenter
 	}
-}
-
-// MARK: - Module Protocol
-
-extension ___FILEBASENAMEASIDENTIFIER___Module: ModuleProtocol {
-	var viewController: UIViewController { return view }
 }
