9173af6cec41acdc8660a97256830708db4f8cbf
Cody Winton <cody.t.winton@gmail.com>
2018-02-04 22:12:49 -0600

    Updated Presenter templates

--- Templates/VIPER/Module Storyboard.xctemplate/___FILEBASENAME___Presenter.swift
+++ Templates/VIPER/Module Storyboard.xctemplate/___FILEBASENAME___Presenter.swift
@@ -33,7 +33,7 @@ protocol ___VARIABLE_productName___InteractorPresenterProtocol: class {
 // MARK: -
 
 /// The Presenter for the ___VARIABLE_productName___ module
-final class ___FILEBASENAMEASIDENTIFIER___ {
+final class ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ViewPresenterProtocol, ___VARIABLE_productName___InteractorPresenterProtocol {
 
 	// MARK: - Constants
 
@@ -50,20 +50,14 @@ final class ___FILEBASENAMEASIDENTIFIER___ {
 		self.router = router
 		self.interactor = interactor
 	}
-}
 
-// MARK: - ___VARIABLE_productName___ View to Presenter Protocol
-
-extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___ViewPresenterProtocol {
+	// MARK: - ___VARIABLE_productName___ View to Presenter Protocol
 
 	func viewLoaded() {
 		interactor.requestTitle()
 	}
-}
 
-// MARK: ___VARIABLE_productName___ Interactor to Presenter Protocol
-
-extension ___FILEBASENAMEASIDENTIFIER___: ___VARIABLE_productName___InteractorPresenterProtocol {
+	// MARK: - ___VARIABLE_productName___ Interactor to Presenter Protocol
 
 	func set(title: String?) {
 		view?.set(title: title)
